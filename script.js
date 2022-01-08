f58876572c232746a263036f754d5c8bccd3d988
unknown <hajar8328@gmail.com>
2022-01-08 23:02:58 +0100

    'final'

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 
 function ageCalculator() {
-    var today = new Date();
-    var userinput = document.getElementById("p1").value;  
-    var birthday = new Date(userinput);  
-    if(userinput==null || userinput=='') {  
+
+    var birthday = document.getElementById("p1").value;  
+    var arr = birthday.split("-");
+
+    var d1 = arr[2];
+    var m1 = arr[1];
+    var y1 = arr[0];
+
+    if(birthday==null || birthday=='') {  
       document.getElementById("result").innerHTML = "**Choose a date please!";    
       return false;   
-    } else {  
-            var age = today.getFullYear() - birthday.getFullYear();
-            var m = today.getMonth() - birthday.getMonth();
-            var d = today.getDay() - birthday.getDay();
-                var b = Math.abs(m);  
 
-            if (b < 0 || (b === 0 && today.getDate() < birthday.getDate())) {
-        age--;
+    } else {  
+        var date = new Date();
+        var d2 = date.getDate();
+        var m2 = 1 + date.getMonth();
+        var y2 = date.getFullYear();
+
+        var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+        if(d1 > d2){
+        d2 = d2 + month[m2 - 1];
+        m2 = m2 - 1;
+        }
+
+        if(m1 > m2){
+        m2 = m2 + 12;
+        y2 = y2 - 1;
+        }
+
+        var d = d2 - d1;
+        var m = m2 - m1;
+        var y = y2 - y1;
+
+
     }
 
         let output = '';
-        output = `<div class="result">${age} year </br> ${b} mounth </br> ${d} days </div>`;
+        output = `<div class="result">${y} year </br> ${m} mounth </br> ${d} days </div>`;
         document.getElementById('div1').innerHTML = output;
 
 }
 
-}
